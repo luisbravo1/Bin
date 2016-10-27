89ac74ac34f6ecd79f308b2d61de52dc57b43f3b
Luis Bravo <luis.g.bravo1@gmail.com>
2016-10-27 16:44:29 -0500

    Added final version

--- studyMath.cpp
+++ studyMath.cpp
@@ -1,23 +1,62 @@
 /*
 Luis Gerardo Bravo Ramones A01282014
+
+Casos de prueba:
+
+Cuantos elementos usara? 5
+Ingresar numero 1: 3
+Ingresar numero 2: 6
+Ingresar numero 3: 90
+Ingresar numero 4: 3
+Error, porfavor ingresa un numero valido
+Ingresar numero 4: 96
+Ingresar numero 5: 98
+Los valores son:
+3
+6
+90
+96
+98
+
+Cuantos elementos usara? 3
+Ingresar numero 1: -15
+Error, porfavor ingresa un numero valido
+Ingresar numero 1: 40
+Ingresar numero 2: 80
+Ingresar numero 3: 110
+Error, porfavor ingresa un numero valido
+Ingresar numero 3: 100
+Los valores son:
+40
+80
+100
 */
 
 #include <iostream>
 using namespace std;
 
-
-void leeDatos (int dArrNum[], int n)
+//Funcion que lee la cantidad de datos para el arreglo
+void leeDatos (int dArrNum[], int n) 
 {
 	for (int i=0; i < n; i++) 
 	{
 		cout << "Ingresar numero " << i+1 << ": ";
 		cin >> dArrNum[i];
-		if (dArrNum[i] <= dArrNum[i-1])
+
+		while ((dArrNum[i] <= dArrNum[i-1]) || (dArrNum[i] < 0) || (dArrNum[i] > 100)) //Para verificar que el numero no sea menor al anterior
 		{
 			cout << "Error, porfavor ingresa un numero valido" << endl;
 			cout << "Ingresar numero " << i+1 << ": ";
 			cin >> dArrNum[i];
 		}
+	}
+}
+
+void mostrarDatos (int dArrNum[], int n)
+{
+	for (int i=0; i < n; i++)
+	{
+		cout << dArrNum[i] << endl;
 	}
 }
 
@@ -29,6 +68,8 @@ int main()
 	cin >> n;
 
 	leeDatos(ArrNumeros, n);
+	cout << "Los valores son: " << endl;
+	mostrarDatos(ArrNumeros, n);
 
 	return 0;
 }
