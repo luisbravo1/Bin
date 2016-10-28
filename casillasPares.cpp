4aa9b5a5f9768fc7b1a75f24b37406f07b453f0d
Luis Bravo <luis.g.bravo1@gmail.com>
2016-10-28 09:34:54 -0500

    Added initial commmit

---
+++ casillasPares.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+void leeDatos (int iArrNum[], int iTam) 
+{
+	for (int i=0; i < iTam; i++) 
+	{
+		cout << "Ingresar numero " << i << ": ";
+		cin >> iArrNum[i];
+	}
+}
+
+void casillasPares (int iArrA[], int iTamA, int iArrB[], int &iTamB)
+{
+	for (int i=0; i < iTamA; i++) 
+	{
+		if (i % 2 == 0)
+		{
+			iArrB[iTamB] = iArrA[i];
+			iTamB++;
+		}
+	}
+}
+
+void mostrarDatos (int iArrNum[], int iTam)
+{
+	for (int i=0; i < iTam; i++)
+	{
+		cout << iArrNum[i] << endl;
+	}
+}
+
+int main()
+{
+	int iArrA[20], iTamA, iArrB[20], iTamB = 0;
+
+	cout << "Cuantos elementos usara? ";
+	cin >> iTamA;
+	leeDatos(iArrA, iTamA);
+	casillasPares(iArrA, iTamA, iArrB, iTamB);
+	cout << "Los numeros en las casillas pares son: " << endl;
+	mostrarDatos(iArrB, iTamB);
+
+	return 0;
+}
