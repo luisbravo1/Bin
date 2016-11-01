4d6d2868af0dc155d7d6b22e7bccac996ad193d4
Luis Bravo <luis.g.bravo1@gmail.com>
2016-11-01 09:51:47 -0600

    Added functions

--- matriz.cpp
+++ matriz.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main() 
+void leeDatos (int iMatNum[10][10], int &iSizeR, int &iSizeC)
 {
-	int iMatNum[10][10];
-	int iSizeR, iSizeC;
-
 	cout << "Cuantos renglones: ";
 	cin >> iSizeR;
 	cout << "Cuantas columnas: ";
@@ -18,15 +15,28 @@ int main()
 			cin >> iMatNum[reng][col];
 		}
 	}
+}
 
+void mostrarDatos (int iMatNum[10][10], int &iSizeR, int &iSizeC)
+{
 	for (int reng=0; reng < iSizeR; reng++) {
-
 		for (int col=0; col < iSizeC; col++)
 		{
-			cout << iMatNum[reng][col] << " ";
+			cout << iMatNum[reng][col] << "\t";
 		}
 		cout << endl;
 	}
+
+}
+
+int main() 
+{
+	int iMatNum[10][10];
+	int iSizeR, iSizeC;
+
+	leeDatos(iMatNum, iSizeR, iSizeC);
+	mostrarDatos(iMatNum, iSizeR, iSizeC);
+
 
 	return 0;
 }
