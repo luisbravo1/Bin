fbf7836d745c18d4fe0bf79ccb988eefcc8e0b1c
Luis Bravo <luis.g.bravo1@gmail.com>
2016-11-02 22:37:39 -0600

    Added player turns

--- blobs.cpp
+++ blobs.cpp
@@ -14,10 +14,15 @@ void mostrarTablero (char MatTablero[6][6]) //Muestra el tablero cada turno
 
 }
 
-void leeSeleccion (char MatTablero[6][6], int &selecRen, int &selecCol, bool seleccion) //Lee los datos de seleccion
+void leeSeleccion (char MatTablero[6][6], int &selecRen, int &selecCol, bool seleccion, char &cPlayer) //Lee los datos de seleccion
 {
+	int jugador;
+	if (cPlayer == 'X')
+		jugador = 1;
+	else 
+		jugador = 2;
 
-	cout << "Jugador 1 X" << endl;
+	cout << "Jugador " << jugador << " " << cPlayer << endl;
 
 	while (seleccion == false){ //Verificar que la seleccion sea correcta
 		cout << "Renglon -> ";
@@ -25,15 +30,12 @@ void leeSeleccion (char MatTablero[6][6], int &selecRen, int &selecCol, bool sel
 		cout << "Coumna -> ";
 		cin >> selecCol;
 
-		if (MatTablero[selecRen][selecCol] == 'X'){
+		if (MatTablero[selecRen][selecCol] == cPlayer)
 			seleccion = true;
-		} else {
-			seleccion = false;
-		}
 	}
 }
 
-void movimiento (int selecRen, int selecCol, bool seleccion, char MatTablero[6][6]) //Mueve al jugador
+void movimiento (int selecRen, int selecCol, bool seleccion, char MatTablero[6][6], char cPlayer) //Mueve al jugador
 {
 	char destino;
 
@@ -44,41 +46,41 @@ void movimiento (int selecRen, int selecCol, bool seleccion, char MatTablero[6][
 
 	switch (destino){
 		case 'a':
-			if (MatTablero[selecRen][selecCol-1] == '_')
-				MatTablero[selecRen][selecCol-1] = 'X';
+			if (MatTablero[selecRen][selecCol-1] == '_') 	 //Se ponen limites en el movimiento, no pueden moverse si
+				MatTablero[selecRen][selecCol-1] = cPlayer;	 //no esta vacia la casilla
 			break;
 		case 'x':
 			if (MatTablero[selecRen+1][selecCol] == '_')
-				MatTablero[selecRen+1][selecCol] = 'X';
+				MatTablero[selecRen+1][selecCol] = cPlayer;
 			break;
 		case 'w':
 			if (MatTablero[selecRen-1][selecCol] == '_')
-				MatTablero[selecRen-1][selecCol] = 'X';
+				MatTablero[selecRen-1][selecCol] = cPlayer;
 			break;
 		case 'd':
 			if (MatTablero[selecRen][selecCol+1] == '_')
-				MatTablero[selecRen][selecCol+1] = 'X';
+				MatTablero[selecRen][selecCol+1] = cPlayer;
 			break;
 		case 'q':
 			if (MatTablero[selecRen-1][selecCol-1] == '_')
-				MatTablero[selecRen-1][selecCol-1] = 'X';
+				MatTablero[selecRen-1][selecCol-1] = cPlayer;
 			break;
 		case 'e':
 			if (MatTablero[selecRen-1][selecCol+1] == '_')
-				MatTablero[selecRen-1][selecCol+1] = 'X';
+				MatTablero[selecRen-1][selecCol+1] = cPlayer;
 			break;
 		case 'z':
 			if (MatTablero[selecRen+1][selecCol-1] == '_')
-				MatTablero[selecRen+1][selecCol-1] = 'X';
+				MatTablero[selecRen+1][selecCol-1] = cPlayer;
 			break;
 		case 'c':
 			if (MatTablero[selecRen+1][selecCol+1] == '_')
-				MatTablero[selecRen+1][selecCol+1] = 'X';
+				MatTablero[selecRen+1][selecCol+1] = cPlayer;
 			break;
 	}
 }
 
-void seguirJugando (bool &continuar)
+void seguirJugando (bool &continuar) //Verifica si quiere seguir jugando el usuario
 {
 	char respuesta;
 
@@ -94,20 +96,33 @@ void seguirJugando (bool &continuar)
 	} while ((respuesta != 'n') && (respuesta != 's'));
 }
 
+void turnoJugador (bool &player1, char &cPlayer) //Cambia el turno del jugador 1 o jugador 2
+{
+	if (player1 == false)
+		cPlayer = 'X';
+	else
+		cPlayer = 'O';
+
+	player1 = !player1;
+
+}
+
 int main() 
 {
 	char MatTablero[6][6] = {{'0','1','2','3','4','5'},{'1','X','_','_','_','X'},{'2','_','_','_','_','_'},\
 	{'3','_','_','_','_','_'},{'4','_','_','_','_','_'},{'5','O','_','_','_','O'}};
 	int selecRen, selecCol;
 	bool seleccion = false, continuar = true, player1 = true;
+	char cPlayer = 'X';
+
+	mostrarTablero(MatTablero);
 
 	do {
-		mostrarTablero(MatTablero);
-		leeSeleccion(MatTablero, selecRen, selecCol, seleccion);
-		movimiento(selecRen, selecCol, seleccion, MatTablero);
+		leeSeleccion(MatTablero, selecRen, selecCol, seleccion, cPlayer);
+		movimiento(selecRen, selecCol, seleccion, MatTablero, cPlayer);
 		mostrarTablero(MatTablero);
 		seguirJugando(continuar);
-		player1 = !player1;
+		turnoJugador(player1, cPlayer);
 	} while (continuar == true);
 
 	return 0;
