e08be11b5ff14a36fdff60a1134dda642b950060
Luis Bravo <luis.g.bravo1@gmail.com>
2017-01-10 12:24:54 -0600

    Not fully working

---
+++ arrayRandom.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int n = 0;
+	int array [10][10]; 
+	
+
+	cout << "How many cells and columns" << endl;
+	cin >> n;
+
+	int y = n / 2, x = 0;
+
+	for (int reng=0; reng < n; reng++) {
+		for (int col=0; col < n; col++)
+		{
+			cout << array[reng][col] << "\t";
+		}
+		cout << endl;
+	}	
+
+	int num = 1;
+	int y2 = 0;
+	int x2 = 0;
+
+	for (int i=0; i < n * n; i++){
+
+		if (array[x-1][y+1] != 0){
+		    y = y2;
+			x = x2;
+			x++;
+			array[x][y] = num;
+
+		} else {
+			y++;
+			x--;
+			array[x][y] = num;
+			
+		    y2 = y;
+		    x2 = x;
+		}
+			
+			
+
+			if (x < 0){
+				x = n - 1;
+			}
+			if (y >= n){
+				y = 0;
+			}
+			if (x >= n){
+				x = 0;
+			}
+			if (y < 0){
+				y = n - 1;
+			}
+			num++;
+	for (int reng=0; reng < n; reng++) {
+		for (int col=0; col < n; col++)
+		{
+			cout << array[reng][col] << "\t";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	cout << endl;
+	cout << endl;
+
+	}	
+}
